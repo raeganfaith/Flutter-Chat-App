695623db6bd13fc838a2084e6dbe9c58acabc0d2
niel-m <srgnne.mendoza@gmail.com>
2023-10-16 18:16:38 +0800

    home page completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,29 @@
 import 'package:chat_app/pages/landing_page.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'chat_page.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  //instance of auth
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  //sign user out
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  bool _isItemDeleted = false;
+
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
     authService.signOut();
@@ -28,24 +33,117 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        actions: [
-          //sign out button
-          IconButton(
-            onPressed: signOut,
-            icon: const Icon(Icons.logout),
-          )
-        ],
-      ),
-      body: _buildUserList(),
-    );
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+    String? imageName = data['image'];
+
+    // Display all users except the current user
+    if (_auth.currentUser!.email != data['email']) {
+      return Slidable(
+        key: ValueKey(data['uid']),
+        endActionPane: ActionPane(
+          motion: DrawerMotion(),
+          children: [
+            SlidableAction(
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              onPressed: (context) async {
+                bool confirmDelete =
+                    await _showDeleteConfirmationModal(context);
+                if (confirmDelete) {
+                  QuerySnapshot querySnapshot = await _fireStore
+                      .collection('users')
+                      .where('email', isEqualTo: _auth.currentUser?.email)
+                      .get();
+
+                  String userID = querySnapshot.docs.first.id;
+                  String otherUserID = data['uid'];
+
+                  // Call the function to delete conversation
+                  ChatService chatService = ChatService();
+                  await chatService.deleteConversation(userID, otherUserID);
+                }
+              },
+            ),
+          ],
+        ),
+        child: ListTile(
+          title: Text(data['username'] ?? 'unknown user'),
+          leading: imageName != null && imageName.isNotEmpty
+              ? CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(imageName),
+                )
+              : const CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatPage(
+                  receiverUserEmail: data['email'],
+                  receiverUserID: data['uid'] ?? '',
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 
-  //build a list of users except for the current logged in user
+  Future<bool> _showDeleteConfirmationModal(BuildContext context) async {
+    bool? result = await showModalBottomSheet<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Delete Conversation',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Text('Are you sure you want to delete this conversation?'),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pop(false); // Dismiss the modal and return false
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pop(true); // Dismiss the modal and return true
+                    },
+                    child: Text('Delete'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return result ?? false;
+  }
+
   Widget _buildUserList() {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('users').snapshots(),
@@ -67,30 +165,20 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  //build indivicual user list items
-  Widget _buildUserListItem(DocumentSnapshot document) {
-    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-
-    //display all users except current user
-    if (_auth.currentUser!.email != data['email']) {
-      return ListTile(
-        title: Text(data['email']),
-        onTap: () {
-          //pass the clicked user's UID to the chat page
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ChatPage(
-                receiverUserEmail: data['email'],
-                receiverUserID: data['uid'],
-              ),
-            ),
-          );
-        },
-      );
-    } else {
-      //return empty container
-      return Container();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            onPressed: signOut,
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: _buildUserList(),
+    );
   }
 }
